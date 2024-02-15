fb3d097efa81aa66b74b82427accb93eb086a74c
Gabriel <gabriel@webcgi.com.br>
2024-02-15 10:26:12 -0300

    Primeira versão de um site com funcionalidades Vue

---
+++ src/components/servicos/Opcoes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h5>Compondente Opções</h5>
+        <p>Poderíamos recuperar as opções do serviço id {{id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Opcoes',
+    props: ['id'],
+    created() {
+        //console.log('Parâmetro via props', this.id)
+    },
+    //beforeRouteUpdate(to, from, next) {
+    beforeRouteUpdate() {
+        console.log('Guarda de componente beforeRouteUpdate')
+    }
+}
+</script>
---
+++ src/views/PaginaNaoEncontrada.vue
@@ -0,0 +1,4 @@
+<template>
+    <h5> -_- Opsss! Essa página não foi encontrada... </h5>
+    
+</template>
