ffe006acfc91b1ae3c6d60329ac27d396f644a96
Gabriel <gabriel@webcgi.com.br>
2024-02-19 10:29:55 -0300

    Adicionando funcionalidade no botão sair na tela

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
                 <div class="navbar-nav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sair</a>
+                            <a class="nav-link" href="/">Sair</a>
                         </li>
                     </ul>
                 </div>
